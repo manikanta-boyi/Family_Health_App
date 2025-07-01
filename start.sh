8383d9ea57968bfde500106e12616636069e8f8d
manikanta-boyi <manikanta.boyi@gmail.com>
2025-07-01 14:57:26 +0530

    Temporary stamp Deploy

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-set -e
-echo ">>> Applying database migrations..."
-flask db upgrade
-echo ">>> Starting the Gunicorn server..."
-exec gunicorn -w 4 -b 0.0.0.0:10000 run:app
+flask db stamp head
+exit 0
