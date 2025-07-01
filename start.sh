942a9b1cababb76bac82edae7e2a2622266b1bb4
manikanta-boyi <manikanta.boyi@gmail.com>
2025-07-01 15:02:44 +0530

    delete and recreated alembic

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-flask db stamp head
-exit 0
+set -e
+echo ">>> Applying database migrations..."
+flask db upgrade
+echo ">>> Starting the Gunicorn server..."
+exec gunicorn -w 4 -b 0.0.0.0:10000 run:app
